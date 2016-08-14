aa02c62ac6e19a0e0e9c9094ef7ef76de629fb7f
makipol@gmail.com <makipol@gmail.com>
2016-08-14 13:58:12 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ImageTyper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Panel2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.splitContainer1.Size = new System.Drawing.Size(670, 404);
+            this.splitContainer1.SplitterDistance = 478;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.IsSplitterFixed = true;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer2.Size = new System.Drawing.Size(188, 404);
+            this.splitContainer2.SplitterDistance = 375;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(188, 375);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "READY\n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(478, 404);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 404);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageTyper
+{
+    public partial class Form1 : Form
+    {
+        private static string _lastCommand;
+        private static string _command;
+        private static Bitmap _bmp;
+        private static IntPtr _scan;
+        private static BitmapData _bmpdata;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _bmp = new Bitmap(512, 512);
+            pictureBox1.Image = _bmp;
+        }
+        #region CommandWorker
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text.Length == 0)
+                    return;
+                TransferCommand(textBox1.Text);
+                textBox1.Clear();
+            }
+            if(e.KeyCode == Keys.Up)
+                if (!string.IsNullOrWhiteSpace(_lastCommand))
+                    textBox1.Text = _lastCommand;
+        }
+
+        private void TransferCommand(string command)
+        {
+            richTextBox1.AppendText(">" + command + "\n");
+            _command = command;
+            _lastCommand = _command;
+            PrepareCommands();
+        }
+
+        private void PrepareCommands()
+        {
+            byte[] b = MarshalImage();
+            Process(b);
+            UpdateImage(b);
+        }
+        #endregion
+
+        internal void Process(byte[] b)
+        {
+            string[] com = _command.ToLower().Split('(');
+            string[] parameters = null;
+            if(com.Length != 1)
+            {
+                parameters = com[1].Split(',');
+                if(parameters[parameters.Length-1].Trim()[parameters[parameters.Length-1].Length-1] == ')')
+                    parameters[parameters.Length - 1] = parameters[parameters.Length - 1].Substring(0, parameters[parameters.Length - 1].Length - 1);
+            }
+
+            if(com[0] == "drawshape" || com[0] == "shape" && parameters.Length == 4)
+            {
+                DrawShape(b, parameters[0], parameters[1], parameters[2], parameters[3]);
+                return;
+            }
+
+        }
+
+        internal void DrawShape(byte[] b, string xx, string yy, string height, string width)
+        {
+            int x = int.Parse(xx) & 511;
+            int y = int.Parse(yy) & 511;
+            int h = Math.Abs(MathExtended.Clamp(int.Parse(height), 511-y));
+            int w = Math.Abs(MathExtended.Clamp(int.Parse(width), 511 - x));
+            int start = GetPixelLocation(x, y);
+            int end = GetPixelLocation(x + w, y + h);
+            int locY = 0, locX = 0;
+            for(int i = start; i<end; i+= 3)
+            {
+                if(locX >= w)
+                {
+                    locY++;
+                    i = GetPixelLocation(x, y + locY);
+                    locX = 0;
+                }
+                b[i] = 0xFF;
+                b[i + 1] = 0xFF;
+                b[i + 2] = 0xFF;
+                locX++;
+            }
+        }
+
+        internal static class MathExtended
+        {
+            public static int Clamp(int value, int maxValue)
+            {
+                if (value > maxValue)
+                    return maxValue;
+                else return value;
+            }
+        }
+
+        #region core
+        internal byte[] MarshalImage()
+        {
+            _bmp = new Bitmap(pictureBox1.Image);
+            _bmpdata = _bmp.LockBits(new Rectangle(0, 0, 512, 512), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            _scan = _bmpdata.Scan0;
+            byte[] b = new byte[_bmpdata.Stride * _bmpdata.Height];
+            Marshal.Copy(_scan, b, 0, b.Length);
+            return b;
+        }
+
+        internal void UpdateImage(byte[] b)
+        {
+            Marshal.Copy(b, 0, _scan, b.Length);
+            _bmp.UnlockBits(_bmpdata);
+            pictureBox1.Image = _bmp;
+        }
+
+        //internal Func<int, int> GetPixelLocation = (int x, int y) => return (y * _bmpdata.Stride + x*3);
+
+        internal int GetPixelLocation(int x, int y)
+        {
+            return y * _bmpdata.Stride + x * 3;
+        }
+
+        #endregion
+    }
+}
